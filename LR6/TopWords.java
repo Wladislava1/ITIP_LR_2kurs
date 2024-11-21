f58bf93b9d4bf0e6ae885a4099af6adc320f9fb7
Wladislava1 <146841829+Wladislava1@users.noreply.github.com>
2024-11-21 18:26:16 +0300

    fix LR6 (update file.txt and update TopWords)

--- LR6/TopWords.java
+++ LR6/TopWords.java
@@ -7,23 +7,43 @@ public class TopWords {
         String filePath = "C:\\Git\\ITIP\\ITIP_LR_2kurs\\LR6\\text.txt";
         File file = new File(filePath);
         Map<String, Integer> wordCount = new HashMap<>();
-        try (Scanner scanner = new Scanner(file)) {
+
+        try {
+            Scanner scanner = new Scanner(file);
+            
             while (scanner.hasNext()) {
-                String word = scanner.next().toLowerCase().replaceAll("[^a-zA-Zа-яА-Я0-9]", "");
+                String word = scanner.next().toLowerCase();
+                word = word.replaceAll("[^a-zA-Zа-яА-Я0-9]", ""); 
+                
                 if (!word.isEmpty()) {
-                    wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
+                    if (wordCount.containsKey(word)) {
+                        wordCount.put(word, wordCount.get(word) + 1);
+                    } else {
+                        wordCount.put(word, 1);
+                    }
                 }
             }
+            scanner.close(); 
         } catch (FileNotFoundException e) {
             System.out.println("Файл не найден: " + filePath);
             return;
         }
 
-        List<Map.Entry<String, Integer>> list = new ArrayList<>(wordCount.entrySet());
-        list.sort((entry1, entry2) -> entry2.getValue().compareTo(entry1.getValue()));
+        List<Map.Entry<String, Integer>> wordList = new ArrayList<>(wordCount.entrySet());
+
+        for (int i = 0; i < wordList.size() - 1; i++) {
+            for (int j = i + 1; j < wordList.size(); j++) {
+                if (wordList.get(i).getValue() < wordList.get(j).getValue()) {
+                    Map.Entry<String, Integer> temp = wordList.get(i);
+                    wordList.set(i, wordList.get(j));
+                    wordList.set(j, temp);
+                }
+            }
+        }
+
         System.out.println("Топ-10 самых частых слов:");
-        for (int i = 0; i < Math.min(10, list.size()); i++) {
-            Map.Entry<String, Integer> entry = list.get(i);
+        for (int i = 0; i < Math.min(10, wordList.size()); i++) {
+            Map.Entry<String, Integer> entry = wordList.get(i);
             System.out.println((i + 1) + ". " + entry.getKey() + " - " + entry.getValue() + " раз(а)");
         }
     }
